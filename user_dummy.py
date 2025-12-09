94312b849255da8e0043ea9cd1d2935b279afe8a
dominik <dominik.sukic@wwb.de>
2025-12-09 15:14:50 +0100

    update user dummy to work with new API

--- user_dummy.py
+++ user_dummy.py
@@ -1,36 +1,39 @@
 from krepo.API import KnowledgeRepoAPI
+from model_dummy import MyTorchModule
+
 
 krepo: KnowledgeRepoAPI = KnowledgeRepoAPI(
     tracking_uri="http://localhost:2035",
 )
-krepo.set_experiment("Test_NAS_Experiment")
 
 nas_conf = {
     "optimizer": "adam",
     "layers": 5,
 }
-
 search_conf = {
     "type": "grid",
     "range": [1, 10],
 }
 
-
-run_tags = {
-        "hw_platform": "pico"
-}
-
-krepo.start_run(
+krepo.set_experiment(
+    experiment_name="Test_NAS_Experiment",
     nas_config=nas_conf,
     search_space_config=search_conf,
-    tags=run_tags,
-    run_name="test run3"
+    hw_platform="pico"
 )
-
+krepo.start_run(
+    run_name="test run"
+)
 krepo.log_metrics({"accuracy": 0.8}, step=1)
 krepo.log_metrics({"accuracy": 0.85}, step=2)
 krepo.log_metrics({"accuracy": 0.87}, step=4)
 
+model = MyTorchModule(10, 20, 5)
+model_params = {
+    "task_type": "Regression",
+    "model_family": "Linear",
+    "data_domain": "Timeseries"
+    }
 model_architecture = {
     "1_layer" : {
         "linear": {
@@ -39,7 +42,8 @@ model_architecture = {
             }
         }
     }
-krepo.log_model("CNN", model_architecture)
+krepo.log_model(model_params, model_architecture, model)
+
 krepo.log_metrics({"final_accuracy": 0.9})
 
 krepo.end_run()
